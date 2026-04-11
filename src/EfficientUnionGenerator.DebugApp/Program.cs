c30cf1f759790deca20b69c5613ec3bfd2dcc3a4
Daishi Nakase <nakase.d1220@gmail.com>
2026-04-11 22:29:08 +0900

    initial commit

---
+++ src/EfficientUnionGenerator.DebugApp/Program.cs
@@ -0,0 +1,35 @@
+// See https://aka.ms/new-console-template for more information
+
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
+using EfficientUnion;
+
+Console.WriteLine();
+
+
+namespace FooBar
+{
+    [EfficientUnion()]
+    public readonly partial struct MyUnion1
+    {
+        public partial MyUnion1(int x);
+        public partial MyUnion1(float x);
+        public partial MyUnion1(string x);
+    }
+
+    [EfficientUnion(UnmanagedFieldBitMaskMode.Auto, 0x80000000)]
+    public readonly partial struct MyUnion2
+    {
+        public partial MyUnion2(int x);
+        public partial MyUnion2(float x);
+    }
+
+    [EfficientUnion(UnmanagedFieldBitMaskMode.Explicit, 0x80000000)]
+    public readonly partial struct MyUnion3
+    {
+        [EnumBitPattern(0x00000000)] public partial MyUnion3(int x);
+        [EnumBitPattern(0x80000000)] public partial MyUnion3(float x);
+    }
+}
+
+
